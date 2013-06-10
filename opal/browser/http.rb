5bd6b7858aa76b5e283c52ecfd5657c05fd4d3a1
meh <meh@schizofreni.co>
2013-06-10 18:42:02 +0200

    http: fix request helpers

--- opal/browser/http.rb
+++ opal/browser/http.rb
@@ -8,15 +8,23 @@ module Browser
 
 module HTTP
   def self.get(url, &block)
-    Request.open(:get, url, &block)
+    Request.new(&block).open(:get, url).send
   end
 
   def self.head(url, &block)
-    Request.open(:head, url, &block)
+    Request.new(&block).open(:head, url).send
   end
 
-  def self.post(url, &block)
-    Request.open(:post, url, &block)
+  def self.post(url, data = nil, &block)
+    Request.new(&block).open(:post, url).send(data)
+  end
+
+  def self.put(url, data = nil, &block)
+    Request.new(&block).open(:put, url).send(data)
+  end
+
+  def self.delete(url, data = nil, &block)
+    Request.new(&block).open(:delete, url).send(data)
   end
 end
 
