4f24feea1bd673b195b228b360226bf2ed1a087c
meh <meh@schizofreni.co>
2013-08-02 12:42:37 +0200

    spec: add some event specs

--- spec/dom/event_spec.rb
+++ spec/dom/event_spec.rb
@@ -24,5 +24,68 @@ describe Browser::DOM::Event do
       elem.trigger 'mouse:down'
       count.should == 2
     end
+
+    it "listens for custom events" do
+      count = 0
+      elem  = $document["event-spec"]
+
+      elem.on 'huehue' do
+        count += 1
+      end
+
+      count.should == 0
+      elem.trigger 'huehue'
+      count.should == 1
+      elem.trigger 'huehue'
+      count.should == 2
+    end
+
+    async "passes an event to the handler" do
+      elem = $document["event-spec"]
+
+      elem.on :click do |event|
+        run_async {
+          event.should be_kind_of Browser::DOM::Event
+        }
+      end
+
+      elem.trigger :click
+    end
+
+    async "passes additional arguments to the handler" do
+      elem = $document["event-spec"]
+
+      elem.on :bazinga do |event, foo, bar, baz|
+        run_async {
+          foo.should == 1
+          bar.should == 2
+          baz.should == 3
+        }
+      end
+
+      elem.trigger :bazinga, 1, 2, 3
+    end
+  end
+
+  describe "#off" do
+    it "removes all the handlers that were added" do
+      count = 0
+      elem  = $document["event-spec"]
+
+      elem.on :click do
+        count += 1
+      end
+
+      elem.on :click do
+        count += 1
+      end
+
+      elem.trigger :click
+      count.should == 2
+
+      elem.off :click
+      elem.trigger :click
+      count.should == 2
+    end
   end
 end
