a0185b11753533ab95e9a1931fecabcc5395deea
meh <meh@schizofreni.co>
2013-08-03 11:48:26 +0200

    http: add synchronous helpers

--- opal/browser/http.rb
+++ opal/browser/http.rb
@@ -29,6 +29,30 @@ module HTTP
   def self.delete(url, data = nil, &block)
     send(:delete, url, data, &block)
   end
+
+  def self.send!(method, url, data = nil, &block)
+    Request.new(&block).open(method, url, false).send(data)
+  end
+
+  def self.get!(url, &block)
+    send!(:get, url, &block)
+  end
+
+  def self.head!(url, &block)
+    send!(:head, url, &block)
+  end
+
+  def self.post!(url, data = nil, &block)
+    send!(:post, url, data, &blocK)
+  end
+
+  def self.put!(url, data = nil, &block)
+    send!(:put, url, data, &block)
+  end
+
+  def self.delete!(url, data = nil, &block)
+    send!(:delete, url, data, &block)
+  end
 end
 
 end
