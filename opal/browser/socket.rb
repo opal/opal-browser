7349744b8a713e4cb89bd610c2b350cdb220e1a6
meh <meh@schizofreni.co>
2013-08-13 15:39:45 +0200

    socket: initial implementation

---
+++ opal/browser/socket.rb
@@ -0,0 +1,89 @@
+#--
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+#++
+
+module Browser
+
+class Socket
+  include Native::Base
+  include IO::Writable
+  include DOM::Event::Target
+
+  def initialize(url, protocol = nil, &block)
+    if native?(url)
+      super(url)
+    else
+      super(`new WebSocket(#{url.to_s}, #{protocol.to_n})`)
+    end
+
+    block.call(self) if block
+  end
+
+  alias_native :protocol, :protocol
+  alias_native :url, :url
+  alias_native :buffered, :bufferedAmount
+
+  def type
+    %x{
+      switch (#@native.binaryType) {
+        case "blob":
+          return "blob";
+
+        case "arraybuffer":
+          return "buffer";
+
+        default:
+          return "string";
+      }
+    }
+  end
+
+  def state
+    %x{
+      switch (#@native.readyState) {
+        case window.WebSocket.CONNECTING:
+          return "connecting";
+
+        case window.WebSocket.OPEN:
+          return "open";
+
+        case window.WebSocket.CLOSING:
+          return "closing";
+
+        case window.WebSocket.CLOSED:
+          return "closed";
+      }
+    }
+  end
+
+  def extensions
+    %x{
+      if (#@native.extensions == "") {
+        return nil;
+      }
+      else {
+        return #@native.extensions;
+      }
+    }
+  end
+
+  def alive?
+    state == :open
+  end
+
+  def write(data)
+    `#@native.send(#{data.to_n})`
+  end
+
+  def close(code = nil, reason = nil)
+    `#@native.close(#{code.to_n}, #{reason.to_n})`
+  end
+end
+
+end
