5d41aa538773e20950c03e0f32ec001b96e09d3a
meh <meh@schizofreni.co>
2013-09-09 12:13:07 +0200

    spec/dom: add Element#class_names specs

--- spec/dom/element_spec.rb
+++ spec/dom/element_spec.rb
@@ -10,4 +10,21 @@ describe Browser::DOM::Element do
       $document["lol"].id.should == 'lol'
     end
   end
+
+  describe '#class_names' do
+    html <<-HTML
+      <div id="class-names">
+        <div id="class-names-1" class="a b c"></div>
+        <div id="class-names-2" class=""></div>
+      </div>
+    HTML
+
+    it 'gives an empty array when no class is set' do
+      $document["class-names-2"].class_names.should == []
+    end
+
+    it 'gives an array of class names' do
+      $document["class-names-1"].class_names.should == %w[a b c]
+    end
+  end
 end
