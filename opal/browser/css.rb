29d8784db3de9a9ce0fad030455b3f33c4d36e49
meh <meh@schizofreni.co>
2013-09-18 13:55:11 +0200

    css: add helper to create a CSS style

--- opal/browser/css.rb
+++ opal/browser/css.rb
@@ -1,3 +1,31 @@
 require 'browser/css/style_sheet'
 require 'browser/css/declaration'
 require 'browser/css/rule'
+
+module Browser
+
+module CSS
+  def self.create(text = nil, &block)
+    style = $document.create_element(:style)
+    style[:type] = 'text/css'
+    style.append_to($document.head)
+
+    if text
+      style.inner_text = text
+    end
+
+    if block
+      sheet = $document.style_sheets.last
+
+      if block.arity == 0
+        sheet.instance_eval(&block)
+      else
+        block.call(sheet)
+      end
+    end
+
+    style
+  end
+end
+
+end
