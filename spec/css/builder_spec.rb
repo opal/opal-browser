2e5df73f2eed9fc099d37d3e422c282147634a86
meh <meh@schizofreni.co>
2013-09-20 15:56:13 +0200

    spec/css/builder: add some specs

---
+++ spec/css/builder_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Browser::CSS::Builder do
+  it 'builds a rule' do
+    css = Browser::CSS::Builder.new do
+      rule '#lol' do
+        color 'black'
+      end
+    end
+
+    css.to_s.should == "#lol {\ncolor: black;\n}\n\n"
+  end
+end
