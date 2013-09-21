7fd03c4509e5253cd2726c3587ba580257bff328
meh <meh@schizofreni.co>
2013-09-21 13:56:33 +0200

    css/rule/style: redirect calls to the declaration

--- opal/browser/css/rule/style.rb
+++ opal/browser/css/rule/style.rb
@@ -6,6 +6,10 @@ class Style < Rule
   def declaration
     Declaration.new(`#@native.style`)
   end
+
+  def method_missing(*args, &block)
+    declaration.__send__(*args, &block)
+  end
 end
 
 end; end; end
