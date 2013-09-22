9f9b058e5d1f4d7e666856c04268847f7d34bd93
meh <meh@schizofreni.co>
2013-09-23 01:50:02 +0200

    navigator: bunch of improvements

--- opal/browser/navigator.rb
+++ opal/browser/navigator.rb
@@ -1,48 +1,149 @@
 module Browser
 
+# Class that represents the browser attributes.
 class Navigator
   include Native::Base
 
   Version = Struct.new(:major, :minor, :build)
   Product = Struct.new(:name, :version)
-  Vendor = Struct.new(:name, :version)
+  Vendor  = Struct.new(:name, :version)
 
+  # Class that represents a MIME type.
+  class MimeType
+    include Native::Base
+
+    # Get the plugin for the MIME type.
+    #
+    # @return [Plugin]
+    def plugin
+      Plugin.new(`#@native.enabledPlugin`)
+    end
+
+    # Get the description for the MIME type.
+    #
+    # @return [String]
+    alias_native :description
+
+    # Extensions for this MIME type.
+    #
+    # @return [Array<String>]
+    def extensions
+      `#@native.suffixes`.split(/\s*/)
+    end
+
+    # The MIME type.
+    #
+    # @return [String]
+    alias_native :type
+  end
+
+  # Class to represent a browser plugin.
+  class Plugin < Native::Array
+    def initialize(plugin)
+      super plugin do |m|
+        MimeType.new(m)
+      end
+    end
+
+    # Get the plugin description.
+    #
+    # @return [String]
+    alias_native :description
+
+    # Get the file associated to the plugin.
+    #
+    # @return [String]
+    alias_native :file, :filename
+
+    # Get the plugin name.
+    #
+    # @return [String]
+    alias_native :name
+
+    # Get the plugin version.
+    #
+    # @return [String]
+    alias_native :version
+  end
+
+  # Get the browser code name.
+  #
+  # @return [String]
   alias_native :code, :appCodeName
+
+  # Get the browser name.
+  #
+  # @return [String]
   alias_native :name, :appName
 
+  # Get the browser {Version}.
+  #
+  # @return [Version]
   def version
     Version.new(`#@native.appVersion`, `#@native.appMinorVersion`, `#@native.buildID`)
   end
 
+  # Check if cookies are enabled.
   alias_native :cookies?, :cookieEnabled
 
+  # Check if DNT is disabled.
   def track?
     `!#@native.doNotTrack`
   end
 
-  alias_native :language, :language
-  alias_native :mime_types, :mimeTypes
+  # Check the browser language.
+  alias_native :language
 
+  # Check the supported MIME types.
+  #
+  # @return [Native::Array<MimeType>]
+  def mime_types
+    Native::Array.new `#@native.mimeTypes`, get: :item, named: :namedItem do |m|
+      MimeType.new(m)
+    end
+  end
+
+  # Check if the browser is in offline mode.
   def offline?
     `!#@native.onLine`
   end
 
+  # Get the operating system the browser is running on.
+  #
+  # @return [String]
   alias_native :operating_system, :oscpu
-  alias_native :os, :oscpu
-  alias_native :platform, :platform
-  alias_native :plugins, :plugins
-  alias_native :product, :product
 
+  alias os operating_system
+
+  # Get the platform the browser is running on.
+  alias_native :platform
+
+  # Get enabled plugins.
+  #
+  # @return [Native::Array<Plugin>]
+  def plugins
+    Native::Array.new `#@native.plugins` do |p|
+      Plugin.new(p)
+    end
+  end
+
+  # Get the product name and version.
+  #
+  # @return [Product]
   def product
     Product.new(`#@native.product`, `#@native.productSub`)
   end
 
   alias_native :user_agent, :userAgent
 
+  # Get the vendor name and version.
+  #
+  # @return [Vendor]
   def vendor
     Vendor.new(`#@native.vendor`, `#@naive.vendorSub`)
   end
 
+  # Check if Java is enabled.
   def java?
     `#@native.javaEnabled()`
   rescue
