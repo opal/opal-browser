814306ad9f3b60d10d4b1207eaf9f20c2ae03888
meh <meh@schizofreni.co>
2013-09-22 19:15:42 +0200

    doc: add documentation to utils

--- opal/browser/utils.rb
+++ opal/browser/utils.rb
@@ -1,22 +1,35 @@
 class String
-	def encode_uri_component
-		`encodeURIComponent(#{self})`
-	end
+  # Encode as URI component.
+  #
+  # @return [String] the string encoded for usage as URI component
+  def encode_uri_component
+    `encodeURIComponent(#{self})`
+  end
 
-	def encode_uri
-		`encodeURI(#{self})`
-	end
+  # Encode as URI.
+  #
+  # @return [String] the string encoded as URI
+  def encode_uri
+    `encodeURI(#{self})`
+  end
 
-	def decode_uri_component
-		`decodeURIComponent(#{self})`
-	end
+  # Decode as URI component.
+  #
+  # @return [String] the string decoded as URI component
+  def decode_uri_component
+    `decodeURIComponent(#{self})`
+  end
 
-	def decode_uri
-		`decodeURI(#{self})`
-	end
+  # Decode as URI.
+  #
+  # @return [String] the string decoded as URI
+  def decode_uri
+    `decodeURI(#{self})`
+  end
 end
 
 class Proc
+  # Defer the function to be called as soon as possible.
   def defer(*args)
     %x{
       var func = #{self};
