1d0685f1e9ee632b4b22b9c0c28b44597754ed2b
meh <meh@schizofreni.co>
2013-09-24 01:07:57 +0200

    timeout: add documentation

--- opal/browser/timeout.rb
+++ opal/browser/timeout.rb
@@ -1,21 +1,31 @@
 module Browser
 
+# This class wraps `setTimeout`.
 class Timeout
+  # @!attribute [r] after
+  # @return [Number] the seconds after which the block is called
   attr_reader :after
 
+  # Create and start a timeout.
+  #
+  # @param window [Window] the window to start the timeout on
+  # @param time [Number] seconds after which the block is called
   def initialize(window, time, &block)
-    @window = window
+    @window = Native.convert(window)
     @after  = time
     @block  = block
 
     @id = `#@window.setTimeout(#{block.to_n}, time * 1000)`
   end
 
+  # Abort the timeout.
+  #
+  # @return [self]
   def abort
     `#@window.clearTimeout(#@id)`
-  end
 
-  alias stop abort
+    self
+  end
 end
 
 end
