f1bf1c3d0920a0c9ccad0a4a8e0442fb2dd569a8
meh <meh@schizofreni.co>
2013-09-23 22:38:50 +0200

    location: some changes for URI compatibility

--- opal/browser/location.rb
+++ opal/browser/location.rb
@@ -3,29 +3,64 @@ module Browser
 class Location
   include Native::Base
 
+  # Change the location.
   def assign(url)
     `#@native.assign(#{url.to_s})`
   end
 
+  # Reload the page.
+  #
+  # @param force [Boolean] whether to force the reload
   def reload(force = false)
     `#@native.reload(force)`
   end
 
+  # Replace the current URL.
+  #
+  # @param url [String] the URL to go to
   def replace(url)
     `#@native.replace(#{url.to_s})`
   end
 
+  # Convert the location to a string.
   def to_s
     `#@native.toString()`
   end
 
-  %w[hash host hostname href pathname port protocol search].each {|name|
-    alias_native name
-    alias_native "#{name}="
-  }
+  # @!attribute fragment
+  # @return [String] the hash fragment of the location URI
+  alias_native :fragment, :hash
+  alias_native :fragment=, :hash=
 
-  alias path pathname
-  alias path= pathaname=
+  # @!attribute host
+  # @return [String] the host part of the location URI
+  alias_native :host
+  alias_native :host=
+
+  # @!attribute uri
+  # @return [String] the whole location URI
+  alias_native :uri, :href
+  alias_native :uri=, :href=
+
+  # @!attribute path
+  # @return [String] the path part of the location URI
+  alias_native :path, :pathname
+  alias_native :path=, :pathname=
+
+  # @!attribute port
+  # @return [Integer] the port part of the location URI
+  alias_native :port
+  alias_native :port=
+
+  # @!attribute scheme
+  # @return [String] the scheme part of the location URI
+  alias_native :scheme, :protocol
+  alias_native :scheme=, :protocol=
+
+  # @!attribute query
+  # @return [String] the query part of the location URI
+  alias_native :query, :search
+  alias_native :query=, :search=
 end
 
 end
