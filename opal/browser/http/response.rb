166860eaae3bc2e362b9f337b2df5cd340a6ffb3
meh <meh@schizofreni.co>
2013-09-24 17:43:31 +0200

    http/response: some documentation

--- opal/browser/http/response.rb
+++ opal/browser/http/response.rb
@@ -2,27 +2,38 @@ require 'json'
 
 module Browser; module HTTP
 
+# Represents an HTTP response.
 class Response
   include Native::Base
 
   Status = Struct.new(:code, :text)
 
+  # @!attribute [r] request
+  # @return [Request] the request to this response
   attr_reader :request
 
+  # Create a response from a request.
+  #
+  # @param request [Request] the request
   def initialize(request)
     super(request.to_n)
 
     @request = request
   end
 
+  # @!attribute [r] headers
+  # @return [Headers] the response headers
   def headers
     @headers ||= Headers.parse(`#@native.getAllResponseHeaders()`)
   end
 
+  # @!attribute [r] status
+  # @return [Status] the response status
   def status
     Status.new(`#@native.status || nil`, `#@native.statusText || nil`)
   end
 
+  # Checks if the response was successful
   def success?
     if code = status.code
       code >= 200 && code < 300 || code == 304
@@ -31,10 +42,13 @@ class Response
     end
   end
 
+  # Check if the response failed
   def failure?
     !success?
   end
 
+  # @!attribute [r] text
+  # @return [String] the response body as text
   def text
     %x{
       var result = #@native.responseText;
@@ -47,6 +61,8 @@ class Response
     }
   end
 
+  # @!attribute [r] json
+  # @return [Hash, Array] the response body as JSON
   def json
     %x{
       var result = #@native.responseText;
@@ -59,6 +75,8 @@ class Response
     }
   end
 
+  # @!attribute [r] xml
+  # @return [DOM::Document] the response body as DOM document
   def xml
     %x{
       var result = #@native.responseXML;
@@ -71,6 +89,8 @@ class Response
     DOM(`result`)
   end
 
+  # @!attribute [r] binary
+  # @return [Binary] the response body as binary
   def binary
     return unless request.binary?
 
