60a0bb98e53b12462c5a434183bd0ba3292829ae
meh <meh@schizofreni.co>
2013-09-24 17:42:31 +0200

    http/binary: some additions and documentation

--- opal/browser/http/binary.rb
+++ opal/browser/http/binary.rb
@@ -1,22 +1,56 @@
 module Browser; module HTTP
 
+# Represents a binary result from a HTTP response.
 class Binary
-  include Native::Base
+  # @!attribute [r] type
+  # @return [:string, :buffer] the type of binary
+  attr_reader :type
 
-  def initialize(*)
-    super
-
-    if String === @native
+  # Create a binary from a value.
+  #
+  # @param value [String, Buffer] the binary
+  def initialize(value)
+    if String === value
       @type = :string
-      @data = @native
+      @data = value
     else
       @type = :buffer
-      @data = @native.to_a
+      @data = value.to_a
     end
   end
 
+  include Enumerable
+
+  # Iterate over each byte in the binary.
+  #
+  # @yield [byte] the byte
+  # @return [self]
+  def each(&block)
+    return enum_for :each unless block
+
+    index  = 0
+    length = self.length
+
+    while index < length
+      block.call(self[index])
+
+      index += 1
+    end
+
+    self
+  end
+
+  # Access a byte from the binary.
+  #
+  # @return [Integer] a byte
   def [](index)
     @type == :string ? `#@data.charCodeAt(index) & 0xff` : @data[index]
+  end
+
+  # @!attribute [r] length
+  # @return [Integer] the length of the binary
+  def length
+    @data.length
   end
 end
 
