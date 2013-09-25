ff1cd08d13863965721f747137789193cb294ce5
meh <meh@schizofreni.co>
2013-09-25 18:15:27 +0200

    spec/css/builder: add some prefix generation tests

--- spec/css/builder_spec.rb
+++ spec/css/builder_spec.rb
@@ -10,4 +10,25 @@ describe Browser::CSS::Builder do
 
     css.to_s.should == "#lol {\n\tcolor: black;\n}"
   end
+
+  it 'builds border-radius correctly' do
+    css = Browser::CSS::Builder.new do
+      rule '#lol' do
+        border radius: '5px'
+      end
+    end
+
+    css.to_s.should == "#lol {\n\t-moz-border-radius: 5px;\n\t-webkit-border-radius: 5px;\n\tborder-radius: 5px;\n}"
+  end
+
+  it 'builds box-shadow correctly' do
+    css = Browser::CSS::Builder.new do
+      rule '#lol' do
+        box shadow: '0 0 5px black'
+      end
+    end
+
+    css.to_s.should == "#lol {\n\t-moz-box-shadow: 0 0 5px black;\n\t-webkit-box-shadow: 0 0 5px black;\n\tbox-shadow: 0 0 5px black;\n}"
+  end
+
 end
