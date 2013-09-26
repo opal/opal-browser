0bc4ad18ec31b3ba28ee1b86516d3fc761521721
meh <meh@schizofreni.co>
2013-09-26 19:23:15 +0200

    css/definition: add border-radius and box-shadow support

--- opal/browser/css/definition.rb
+++ opal/browser/css/definition.rb
@@ -21,87 +21,81 @@ class Definition
     self
   end
 
+  def border(*args); _border(false, *args); end
+  def border!(*args); _border(true, *args); end
+
+  def _border(important, options)
+    if Hash === options
+      options.each {|name, value|
+        case name
+        when :radius
+          if String === value
+            style('-moz-border-radius', value, important)
+            style('-webkit-border-radius', value, important)
+            style('border-radius', value, important)
+          else
+            value.map {|horizontal, value|
+              value.map {|vertical, value|
+                style("-moz-border-radius-#{horizontal}#{vertical}", value, important)
+                style("-webkit-border-#{horizontal}-#{vertical}-radius", value, important)
+                style("border-#{horizontal}-#{vertical}-radius", value, important)
+              }
+            }
+          end
+
+        else
+          style("border-#{name}", value, important)
+        end
+      }
+    else
+      style(:border, options, important)
+    end
+  end
+
+  def box(*args); _box(false, *args); end
+  def box!(*args); _box(true, *args); end
+
+  def _box(important, options)
+    if Hash === options
+      options.each {|name, value|
+        case name
+        when :shadow
+          if Array === value
+            value = value.join ', '
+          end
+
+          style('-moz-box-shadow', value, important)
+          style('-webkit-box-shadow', value, important)
+          style('box-shadow', value, important)
+
+        else
+          style("box-#{name}", value, important)
+        end
+      }
+    else
+      style(:box, options, important)
+    end
+  end
+
   def method_missing(name, argument, &block)
     important = name.end_with? ?!
     name      = name[0 .. -2] if important
 
     if Hash === argument
       argument.each {|sub, value|
-        @style << Style.new("#{name}-#{sub}", value, important)
+        style("#{name}-#{sub}", value, important)
       }
     else
-      @style << Style.new(name, argument, important)
+      style(name, argument, important)
     end
 
     self
   end
+
+private
+  def style(name, value, important = false)
+    @style << Style.new(name, value, important)
+  end
 end
 
 end; end
-
-=begin
-define :border do |args|
-  return [['border', args]] if String === args
-
-  args.map {|name, value|
-    case name
-    when :radius
-      if String === value
-        [['-moz-border-radius', value],
-         ['-webkit-border-radius', value],
-         ['border-radius', value]]
-      else
-        value.map {|horizontal, value|
-          value.map {|vertical, value|
-            [["-moz-border-radius-#{horizontal}#{vertical}", value],
-             ["-webkit-border-#{horizontal}-#{vertical}-radius", value],
-             ["border-#{horizontal}-#{vertical}-radius", value]]
-          }.flatten(1)
-        }.flatten(1)
-      end
-
-    else
-      [["border-#{name}", value]]
-    end
-  }.flatten(1)
-end
-
-define :box do |args|
-  return [['box', args]] if String === args
-
-  args.map {|name, value|
-    case name
-    when :shadow
-      if Array === value
-        value = value.join ', '
-      end
-
-      if String === value
-        [['-moz-box-shadow', value],
-         ['-webkit-box-shadow', value],
-         ['box-shadow', value]]
-      else
-      end
-
-    else
-      [["box-#{name}", value]]
-    end
-  }.flatten(1)
-end
-
-define :gradient do |type, *args|
-  if Hash === args.last
-    options = args.pop
-  end
-
-  result = []
-
-  if fallback = options[:fallback]
-    result << ['background-color', fallback]
-  else
-    result << ['backround-color', from]
-  end
-
-  result
-end
-=end
