afffa2132ff0bb56ad3e0bccfd1d4631ba3d306f
meh <meh@schizofreni.co>
2013-09-28 00:41:45 +0200

    css/definition: simplify definition of helpers and add some

--- opal/browser/css/definition.rb
+++ opal/browser/css/definition.rb
@@ -21,41 +21,73 @@ class Definition
     self
   end
 
-  def border(*args); _border(false, *args); end
-  def border!(*args); _border(true, *args); end
+  def gradient(*args)
+    Gradient.new(*args)
+  end
 
-  def _border(important, options)
+  def background(*args); _background(false, *args); end
+  def background!(*args); _background(true, *args); end
+
+  def _background(important, *args)
+    if Gradient === args.first
+      if args.length > 1
+        raise NotImplementedError, "multiple gradients not implemented yet"
+      end
+
+      args.first.each {|s|
+        style s.name || 'background-image', s.value, important
+      }
+    else
+      options = args.first
+
+      if String === options
+        style :background, options, important
+      else
+        argument.each {|sub, value|
+          style "background-#{sub}", value, important
+        }
+      end
+    end
+  end
+
+  def border(options)
     if Hash === options
       options.each {|name, value|
         case name
         when :radius
           if String === value
-            style('-moz-border-radius', value, important)
-            style('-webkit-border-radius', value, important)
-            style('border-radius', value, important)
+            style '-moz-border-radius', value
+            style '-webkit-border-radius', value
+            style 'border-radius', value
           else
-            value.map {|horizontal, value|
-              value.map {|vertical, value|
-                style("-moz-border-radius-#{horizontal}#{vertical}", value, important)
-                style("-webkit-border-#{horizontal}-#{vertical}-radius", value, important)
-                style("border-#{horizontal}-#{vertical}-radius", value, important)
+            value.each {|horizontal, value|
+              value.each {|vertical, value|
+                style "-moz-border-radius-#{horizontal}#{vertical}", value
+                style "-webkit-border-#{horizontal}-#{vertical}-radius", value
+                style "border-#{horizontal}-#{vertical}-radius", value
               }
             }
           end
 
+        when :color
+          if String === value
+            style 'border-color', value
+          else
+            value.each {|name, value|
+              style "border-#{name}-color", value
+            }
+          end
+
         else
-          style("border-#{name}", value, important)
+          style "border-#{name}", Array(value).join(' ')
         end
       }
     else
-      style(:border, options, important)
+      style :border, Array(options).join(' ')
     end
   end
 
-  def box(*args); _box(false, *args); end
-  def box!(*args); _box(true, *args); end
-
-  def _box(important, options)
+  def box(options)
     if Hash === options
       options.each {|name, value|
         case name
@@ -64,37 +96,127 @@ class Definition
             value = value.join ', '
           end
 
-          style('-moz-box-shadow', value, important)
-          style('-webkit-box-shadow', value, important)
-          style('box-shadow', value, important)
+          style '-moz-box-shadow', value
+          style '-webkit-box-shadow', value
+          style 'box-shadow', value
 
         else
-          style("box-#{name}", value, important)
+          style "box-#{name}", value
         end
       }
     else
-      style(:box, options, important)
+      style :box, options
     end
   end
 
-  def method_missing(name, argument, &block)
+  def opacity(value)
+    style 'opacity', value
+    style '-moz-opacity', value
+
+    style '-ms-filter', %Q{"progid:DXImageTransform.Microsoft.Alpha(Opacity=#{(value * 100).to_i})"}
+    style 'filter', "alpha(opacity=#{(value * 100).to_i})"
+  end
+
+  def method_missing(name, *args, &block)
     important = name.end_with? ?!
     name      = name[0 .. -2] if important
 
-    if Hash === argument
-      argument.each {|sub, value|
-        style("#{name}-#{sub}", value, important)
-      }
-    else
-      style(name, argument, important)
+    @important = true if important
+
+    if important && respond_to?(name)
+      __send__ name, *args, &block
+      @important = false
+
+      return
     end
+
+    if args.length == 1
+      argument = args.first
+
+      if Hash === argument
+        argument.each {|sub, value|
+          style "#{name}-#{sub}", Array(value).join(' ')
+        }
+      else
+        style name, Array(argument).join(' ')
+      end
+    else
+      style name, args.join(' ')
+    end
+
+    @important = false
 
     self
   end
 
 private
-  def style(name, value, important = false)
-    @style << Style.new(name, value, important)
+  def style(name, value = nil, important = @important)
+    if Style === name
+      @style << name
+    else
+      @style << Style.new(name, value, important)
+    end
+  end
+
+  class Gradient
+    # TODO: all of it, seriously
+    def initialize(*args)
+      options = Hash === args.last ? args.pop : {}
+
+      @to   = options[:to]
+      @from = options[:from]
+
+      if @to && !@from
+        @from = other(@to)
+      elsif @from && !@to
+        @to = other(@from)
+      end
+
+      @start = args.shift
+      @end   = args.shift
+    end
+
+    def each(&block)
+      block.call style("-moz-linear-gradient(#@to, #@start 0%, #@end 100%)")
+
+      if horizontal?
+        block.call style("-webkit-gradient(linear, #@from top, #@to top, color-stop(0%, #@start), color-stop(100%, #@end))")
+      else
+        block.call style("-webkit-gradient(linear, left #@from, left #@to, color-stop(0%, #@start), color-stop(100%, #@end))")
+      end
+
+      block.call style("-webkit-linear-gradient(#@to, #@start 0%, #@end 100%)")
+      block.call style("-o-linear-gradient(#@to, #@start 0%, #@end 100%)")
+      block.call style("-ms-linear-gradient(#@to, #@start 0%, #@end 100%)")
+      block.call style("linear-gradient(to #@to, #@start 0%, #@end 100%)")
+    end
+
+    def horizontal?
+      @to == :left || @to == :right
+    end
+
+    def vertical?
+      @to == :top || @to == :bottom
+    end
+
+  private
+    def other(side)
+      case side
+      when :left   then :right
+      when :right  then :left
+      when :top    then :bottom
+      when :bottom then :top
+      end
+    end
+
+    # FIXME: use default args
+    def style(*args)
+      if args.length == 1
+        Style.new(nil, args.first)
+      else
+        Style.new(*args)
+      end
+    end
   end
 end
 
