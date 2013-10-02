35c241ba919a09ff5d4fa4bc11f169df29d9eb20
meh <meh@schizofreni.co>
2013-10-02 15:22:21 +0200

    dom/compatibility: reorder requires

--- opal/browser/dom/compatibility.rb
+++ opal/browser/dom/compatibility.rb
@@ -1,6 +1,8 @@
+require 'browser/compatibility/dom/document/window'
+
+require 'browser/compatibility/dom/mutation_observer'
+
 require 'browser/compatibility/dom/element/matches'
 require 'browser/compatibility/dom/element/css'
 require 'browser/compatibility/dom/element/offset'
 require 'browser/compatibility/dom/element/style'
-require 'browser/compatibility/dom/document/window'
-require 'browser/compatibility/dom/mutation_observer'
