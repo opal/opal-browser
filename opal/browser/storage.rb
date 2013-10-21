fb2ca7d85d5746462fe298bd1e33452acbce7272
meh <meh@schizofreni.co>
2013-10-21 17:53:54 +0200

    storage: support any kind of key

--- opal/browser/storage.rb
+++ opal/browser/storage.rb
@@ -9,10 +9,19 @@
 #++
 
 require 'json'
+require 'stringio'
 
 module Browser
 
 class Storage < Hash
+  def self.json_create(data)
+    data.delete(JSON.create_id)
+
+    Hash[data.map {|key, value|
+      [JSON.parse(key), value]
+    }]
+  end
+
   attr_reader :name
 
   def initialize(window, name)
@@ -42,9 +51,10 @@ class Storage < Hash
     end
   end
 
-  %w([] []= delete clear).each {|name|
+  %w([]= delete clear).each {|name|
     define_method name do |*args|
-      super.tap {
+      # FIXME: remove the application when it's fixed
+      super(*args).tap {
         save if autosave?
       }
     end
@@ -90,15 +100,29 @@ class Storage < Hash
     end
   else
     def init
-      @document = Document.new(`#@window.document`)
-      @document.cookies.options expires: 60 * 60 * 24 * 365
+      $document.cookies.options expires: 60 * 60 * 24 * 365
 
-      replace @document.cookies[encoded_name]
+      replace $document.cookies[encoded_name]
     end
 
     def save
-      @document.cookies[encoded_name] = self
+      $document.cookies[encoded_name] = self
     end
+  end
+
+  def to_json
+    io = StringIO.new("{")
+
+    io << JSON.create_id.to_json << ":" << self.class.name.to_json << ","
+
+    each {|key, value|
+      io << key.to_json.to_json << ":" << value.to_json << ","
+    }
+
+    io.seek(-1, IO::SEEK_CUR)
+    io << "}"
+
+    io.string
   end
 end
 
