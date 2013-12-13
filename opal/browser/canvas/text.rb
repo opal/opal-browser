7a55ebbcb9cb96c7ba5711e368ea3442f7d7ba79
Jan Svoboda <jan@mluv.cz>
2013-12-13 02:19:03 +0100

    canvas/text: fix max_width in #fill and #stroke

--- opal/browser/canvas/text.rb
+++ opal/browser/canvas/text.rb
@@ -11,30 +11,42 @@
 module Browser; class Canvas
 
 class Text
+  include Native
+
   attr_reader :context
 
   def initialize(context)
     @context = context
+
+    super(@context.to_n)
   end
 
   def measure(text)
     `#@native.measureText(text)`
   end
 
-  def fill(text, x = nil, y = nil, max_width = undefined)
+  def fill(text, x = nil, y = nil, max_width = nil)
     x ||= 0
     y ||= 0
 
-    `#@native.fillText(text, x, y, max_width)`
+    if max_width
+      `#@native.fillText(text, x, y, max_width)`
+    else
+      `#@native.fillText(text, x, y)`
+    end
 
     @context
   end
 
-  def stroke(text, x = nil, y = nil, max_width = undefined)
+  def stroke(text, x = nil, y = nil, max_width = nil)
     x ||= 0
     y ||= 0
 
-    `#@native.strokeText(text, x, y, max_width)`
+    if max_width
+      `#@native.strokeText(text, x, y, max_width)`
+    else
+      `#@native.strokeText(text, x, y)`
+    end
 
     @context
   end
