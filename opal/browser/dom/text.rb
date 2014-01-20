8d65b48662cab17c89fa2b9263a573a642d6833a
meh <meh@schizofreni.co>
2014-01-20 23:40:57 +0100

    dom/text: cleanup and add documentation

--- opal/browser/dom/text.rb
+++ opal/browser/dom/text.rb
@@ -1,16 +1,31 @@
 module Browser; module DOM
 
+# Encapsulates a text node.
+#
+# @see https://developer.mozilla.org/en-US/docs/Web/API/Text
 class Text < CharacterData
+  # (see Document#create_text)
   def self.create(*args)
     $document.create_text(*args)
   end
 
+  # @!attribute [r] whole
+  # @return [String] the whole text
+  #
+  # @see https://developer.mozilla.org/en-US/docs/Web/API/Text.wholeText
   def whole
     `#@native.wholeText`
   end
 
+  # Split the text node at a given offset.
+  #
+  # @param offset [Integer] the offset where to split the text node
+  #
+  # @return [Text] the newly created text node
+  #
+  # @see https://developer.mozilla.org/en-US/docs/Web/API/Text.splitText
   def split(offset)
-    `#@native.splitText(offset)`
+    DOM(`#@native.splitText(offset)`)
   end
 
   def inspect
