0563668e5e466713f6a301a2a2dda3076148d808
meh <meh@schizofreni.co>
2014-01-20 23:32:56 +0100

    dom/attribute: add documentation

--- opal/browser/dom/attribute.rb
+++ opal/browser/dom/attribute.rb
@@ -1,16 +1,22 @@
 module Browser; module DOM
 
+# Encapsulates an {Element} attribute.
 class Attribute
   include Native
 
+  # Returns true if the attribute is an id.
   def id?
     `#@native.isId`
   end
 
+  # @!attribute [r] name
+  # @return [String] the name of the attribute
   def name
     `#@native.name`
   end
 
+  # @!attribute [r] value
+  # @return [String] the value of the attribute
   def value
     `#@native.value`
   end
