13ef6a342209a360f3fee81a51c062fef1b251ca
meh <meh@schizofreni.co>
2014-01-22 16:59:49 +0100

    storage: cleanup code and documentation

--- opal/browser/storage.rb
+++ opal/browser/storage.rb
@@ -1,19 +1,24 @@
-#--
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#                    Version 2, December 2004
-#
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-#
-#  0. You just DO WHAT THE FUCK YOU WANT TO.
-#++
-
 require 'json'
 require 'stringio'
 
 module Browser
 
-class Storage < Hash
+# A {Storage} allows you to store data across page loads and browser
+# restarts.
+#
+# Compatibility
+# -------------
+# The compatibility layer will try various implementations in the following
+# order.
+#
+# + [window.localStorage](https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Storage#localStorage)
+# + [window.globalStorage](https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Storage#globalStorage)
+# + [document.body.addBehavior](http://msdn.microsoft.com/en-us/library/ms531424(VS.85).aspx)
+# + [document.cookie](https://developer.mozilla.org/en-US/docs/Web/API/document.cookie)
+#
+# @see https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Storage
+# @todo remove method_defined? checks when require order is fixed
+class Storage
   def self.json_create(data)
     data.delete(JSON.create_id)
 
@@ -22,94 +27,112 @@ class Storage < Hash
     }]
   end
 
+  include Enumerable
+
+  # @!attribute [r] name
+  # @return [String] the name of the storage
   attr_reader :name
 
+  # Create a new storage on the given window with the given name.
+  #
+  # @param window [native] the window to save the storage to
+  # @param name [String] the name to use to discern different storages
   def initialize(window, name)
     super()
 
     @window = window
     @name   = name
+    @data   = {}
 
     autosave!
-
-    init if respond_to? :init
+    init
   end
 
+  # @!attribute [r] encoded_name
+  # @return [String] the generated name
   def encoded_name
-    "$opal.storage.#{@name}"
+    "$opal.storage.#@name"
   end
 
-  def autosave?;    @autosave;         end
-  def autosave!;    @autosave = true;  end
-  def no_autosave!; @autosave = false; end
+  # Check if autosaving is enabled.
+  #
+  # When autosaving is enabled the {Storage} is saved every time a change is
+  # made, otherwise you'll have to save it manually yourself.
+  def autosave?
+    @autosave
+  end
 
-  def replace(what)
-    if what.is_a?(String)
-      super JSON.parse(what)
+  # Enable autosaving.
+  def autosave!
+    @autosave = true
+  end
+
+  # Disable autosaving.
+  def no_autosave!
+    @autosave = false
+  end
+
+  # Iterate over the (key, value) pairs in the storage.
+  #
+  # @yield [key, value]
+  def each(&block)
+    return enum_for :each unless block
+
+    @data.each(&block)
+
+    self
+  end
+
+  # Get a value in the storage.
+  def [](key)
+    @data[key]
+  end
+
+  # Set a value in the storage.
+  def []=(key, value)
+    @data[key] = value
+
+    save if autosave?
+  end
+
+  # Delete a value from the storage.
+  def delete(key)
+    @data.delete(key).tap {
+      save if autosave
+    }
+  end
+
+  # Clear the storage.
+  def clear
+    @data.clear.tap {
+      save if autosave?
+    }
+  end
+
+  # Replace the current storage with the given one.
+  #
+  # @param new [Hash, String] if new is a {String} it will be parsed as JSON
+  def replace(new)
+    if String === new
+      @data.replace(JSON.parse(new))
     else
-      super
+      @data.replace(new)
     end
   end
 
-  %w([]= delete clear).each {|name|
-    define_method name do |*args|
-      # FIXME: remove the application when it's fixed
-      super(*args).tap {
-        save if autosave?
-      }
-    end
-  }
+  # @private
+  def init
+    replace `#@window.localStorage[#{encoded_name}] || '{}'`
+  end unless method_defined? :init
 
-  def save; end
+  # Save a snapshot of the storage.
+  def save
+    `#@window.localStorage[#{encoded_name}] = #{JSON.dump(self)}`
+  end unless method_defined? :save
 
-  if `window.localStorage`
-    def init
-      replace `#@window.localStorage[#{encoded_name}] || '{}'`
-    end
-
-    def save
-      `#@window.localStorage[#{encoded_name}] = #{JSON.dump(self)}`
-    end
-  elsif `window.globalStorage`
-    def init
-      replace `#@window.globalStorage[#@window.location.hostname][#{encoded_name}] || '{}'`
-    end
-
-    def save
-      `#@window.globalStorage[#@window.location.hostname][#{encoded_name}] = #{JSON.dump(self)}`
-    end
-  elsif `document.body.addBehavior`
-    def init
-      %x{
-        #@element = #@window.document.createElement('link');
-        #@element.addBehavior('#default#userData');
-
-        #@window.document.getElementsByTagName('head')[0].appendChild(#@element);
-
-        #@element.load(#{encoded_name});
-      }
-
-      replace `#@element.getAttribute(#{encoded_name}) || '{}'`
-    end
-
-    def save
-      %x{
-        #@element.setAttribute(#{encoded_name}, #{JSON.dump(self)});
-        #@element.save(#{encoded_name});
-      }
-    end
-  else
-    def init
-      $document.cookies.options expires: 60 * 60 * 24 * 365
-
-      replace $document.cookies[encoded_name]
-    end
-
-    def save
-      $document.cookies[encoded_name] = self
-    end
-  end
-
+  # Convert the storage to JSON.
+  #
+  # @return [String] the JSON representation
   def to_json
     io = StringIO.new("{")
 
@@ -126,6 +149,10 @@ class Storage < Hash
   end
 end
 
+# A {SessionStorage} allows you to store data across page reloads, as long as the session
+# is active.
+#
+# @see https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Storage#sessionStorage
 class SessionStorage < Storage
   def init
     replace `#@window.sessionStorage[#{encoded_name}] || '{}'`
@@ -137,13 +164,25 @@ class SessionStorage < Storage
 end
 
 class Window
+  # Get a storage with the given name.
+  #
+  # @param name [Symbol] the name of the storage
+  #
+  # @return [Storage]
   def storage(name = :default)
     Storage.new(to_n, name)
   end
 
+  # Get a session storage with the given name.
+  #
+  # @param name [Symbol] the name of the storage
+  #
+  # @return [SessionStorage]
   def session_storage(name = :default)
     SessionStorage.new(to_n, name)
   end
 end
 
 end
+
+require 'browser/compatibility/storage'
