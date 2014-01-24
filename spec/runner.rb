9662c9b5404bbf3add141c0e3e269120612c9605
meh <meh@schizofreni.co>
2014-01-24 21:03:54 +0100

    spec: try to be more resilient to random timeouts

--- spec/runner.rb
+++ spec/runner.rb
@@ -3,89 +3,97 @@ require 'selenium/webdriver'
 require 'net/https'
 require 'json'
 
-loop do
-  uri           = URI.parse("https://www.browserstack.com/automate/plan.json")
-  agent         = Net::HTTP.new(uri.host, uri.port)
-  agent.use_ssl = true
-  request       = Net::HTTP::Get.new(uri.request_uri)
-  request.basic_auth(ENV['BS_USERNAME'], ENV['BS_AUTHKEY'])
-
-  state = JSON.parse(agent.request(request).body)
-
-  if state["parallel_sessions_running"] < state["parallel_sessions_max_allowed"]
-    break
-  end
-
-  print '.'
-  sleep 30
-end
-
-puts "\rRunning specs..."
-puts
-
-url = "http://#{ENV['BS_USERNAME']}:#{ENV['BS_AUTHKEY']}@hub.browserstack.com/wd/hub"
-cap = Selenium::WebDriver::Remote::Capabilities.new
-
-cap['platform']        = ENV['SELENIUM_PLATFORM'] || 'ANY'
-cap['browser']         = ENV['SELENIUM_BROWSER'] || 'chrome'
-cap['browser_version'] = ENV['SELENIUM_VERSION'] if ENV['SELENIUM_VERSION']
-
-cap['browserstack.tunnel'] = 'true'
-cap['browserstack.debug']  = 'false'
-
-browser = Selenium::WebDriver.for(:remote, url: url, desired_capabilities: cap)
-browser.navigate.to('http://localhost:9292')
-
-at_exit {
-  browser.quit
-}
+trials = 0
 
 begin
-  Selenium::WebDriver::Wait.new(timeout: 540, interval: 5) \
-    .until { not browser.find_element(:css, 'p#totals').text.strip.empty? }
+  loop do
+    uri           = URI.parse("https://www.browserstack.com/automate/plan.json")
+    agent         = Net::HTTP.new(uri.host, uri.port)
+    agent.use_ssl = true
+    request       = Net::HTTP::Get.new(uri.request_uri)
+    request.basic_auth(ENV['BS_USERNAME'], ENV['BS_AUTHKEY'])
 
-  totals   = browser.find_element(:css, 'p#totals').text
-  duration = browser.find_element(:css, 'p#duration').find_element(:css, 'strong').text
+    state = JSON.parse(agent.request(request).body)
 
-  puts "#{totals} in #{duration}"
-  puts
+    if state["parallel_sessions_running"] < state["parallel_sessions_max_allowed"]
+      break
+    end
 
-  if totals =~ /0 failures/
-    exit 0
+    print '.'
+    sleep 30
   end
 
-  browser.find_elements(:css, '.example_group').slice_before {|x|
-    begin
-      x.find_element(:css, 'dd')
+  puts "\rRunning specs..."
+  puts
 
-      false
-    rescue Exception
-      true
+  url = "http://#{ENV['BS_USERNAME']}:#{ENV['BS_AUTHKEY']}@hub.browserstack.com/wd/hub"
+  cap = Selenium::WebDriver::Remote::Capabilities.new
+
+  cap['platform']        = ENV['SELENIUM_PLATFORM'] || 'ANY'
+  cap['browser']         = ENV['SELENIUM_BROWSER'] || 'chrome'
+  cap['browser_version'] = ENV['SELENIUM_VERSION'] if ENV['SELENIUM_VERSION']
+
+  cap['browserstack.tunnel'] = 'true'
+  cap['browserstack.debug']  = 'false'
+
+  browser = Selenium::WebDriver.for(:remote, url: url, desired_capabilities: cap)
+  browser.navigate.to('http://localhost:9292')
+
+  at_exit {
+    browser.quit
+  }
+
+  begin
+    Selenium::WebDriver::Wait.new(timeout: 120, interval: 5) \
+      .until { not browser.find_element(:css, 'p#totals').text.strip.empty? }
+
+    totals   = browser.find_element(:css, 'p#totals').text
+    duration = browser.find_element(:css, 'p#duration').find_element(:css, 'strong').text
+
+    puts "#{totals} in #{duration}"
+    puts
+
+    if totals =~ /0 failures/
+      exit 0
     end
-  }.each {|header, *specs|
-    next unless specs.any? { |x| x.find_element(:css, '.failed') rescue false }
 
-    namespace = header.find_element(:css, 'dt').text
+    browser.find_elements(:css, '.example_group').slice_before {|x|
+      begin
+        x.find_element(:css, 'dd')
 
-    specs.each {|group|
-      next unless group.find_element(:css, '.failed') rescue false
+        false
+      rescue Exception
+        true
+      end
+    }.each {|header, *specs|
+      next unless specs.any? { |x| x.find_element(:css, '.failed') rescue false }
 
-      method = group.find_element(:css, 'dt').text
+      namespace = header.find_element(:css, 'dt').text
 
-      group.find_elements(:css, 'dd.example.failed').each {|el|
-        puts "#{namespace}#{method}"
-        puts "  #{el.find_element(:css, '.failed_spec_name').text}"
-        puts
-        puts el.find_element(:css, '.failure').text
-        puts
+      specs.each {|group|
+        next unless group.find_element(:css, '.failed') rescue false
+
+        method = group.find_element(:css, 'dt').text
+
+        group.find_elements(:css, 'dd.example.failed').each {|el|
+          puts "#{namespace}#{method}"
+          puts "  #{el.find_element(:css, '.failed_spec_name').text}"
+          puts
+          puts el.find_element(:css, '.failure').text
+          puts
+        }
       }
     }
-  }
+  rescue Selenium::WebDriver::Error::NoSuchElementError
+    puts 'ya blew it'
+    puts browser.page_source
+  end
 rescue Selenium::WebDriver::Error::TimeOutError
-  puts "The specs timed out, some asynchronous spec must have failed, good luck with that."
-rescue Selenium::WebDriver::Error::NoSuchElementError
-  puts 'ya blew it'
-  puts browser.page_source
+  trials += 1
+
+  unless trials >= 4
+    retry
+  end
 end
 
 exit 1
