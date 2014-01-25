3a0910313119b14032a7064553781d39b8d6116f
meh <meh@schizofreni.co>
2014-01-25 14:11:26 +0100

    immediate: inline compatibility

--- opal/browser/immediate.rb
+++ opal/browser/immediate.rb
@@ -19,8 +19,6 @@ module Browser
 #
 # The order has been chosen from best to worst for both performance and
 # preemptiveness.
-#
-# @todo drop the method_defined? checks when require order is fixed
 class Immediate
   # Create an immediate for the given function which will be called with the
   # arguments and block.
@@ -34,19 +32,87 @@ class Immediate
     @block     = block
   end
 
-  # Dispatch the immediate.
-  #
-  # @abstract
-  def dispatch
-    raise NotImplementedError
-  end unless method_defined? :dispatch
+  # @!method dispatch
+  #   Dispatch the immediate.
 
-  # Prevent the immediate from being called once scheduled.
-  #
-  # @abstract
-  def prevent
-    raise NotImplementedError
-  end unless method_defined? :prevent
+  # @!method prevent
+  #   Prevent the immediate from being called once scheduled.
+
+  if Browser.supports? 'Immediate'
+    def dispatch
+      @id = `window.setImmediate(function() {
+        #{@function.call(*@arguments, &@block)};
+      })`
+    end
+
+    def prevent
+      `window.clearImmediate(#@id)`
+    end
+  elsif Browser.supports? 'Immediate (Internet Explorer)'
+    def dispatch
+      @id = `window.msSetImmediate(function() {
+        #{@function.call(*@arguments, &@block)};
+      })`
+    end
+
+    def prevent
+      `window.msClearImmediate(#@id)`
+    end
+  elsif Browser.supports? 'Window.send'
+    # @private
+    @@tasks  = {}
+
+    # @private
+    @@prefix = "opal.browser.immediate.#{rand(1_000_000)}."
+
+    $window.on :message do |e|
+      if String === e.data && e.data.start_with?(@@prefix)
+        if task = @@tasks.delete(e.data[@@prefix.length .. -1])
+          task[0].call(*task[1], &task[2])
+        end
+      end
+    end
+
+    def dispatch
+      @id          = rand(1_000_000).to_s
+      @@tasks[@id] = [@function, @arguments, @block]
+
+      $window.send! "#{@@prefix}#{@id}"
+    end
+
+    def prevent
+      @@tasks.delete(@id)
+    end
+  elsif Browser.supports? 'Event.readystatechange'
+    def dispatch
+      %x{
+        var script = document.createElement("script");
+
+        script.onreadystatechange = function() {
+          if (!#{aborted?}) {
+            #{@function.call(*@arguments, &@block)};
+          }
+
+          script.onreadystatechange = null;
+          script.parentNode.removeChild(script);
+        };
+
+        document.documentElement.appendChild(script);
+      }
+    end
+
+    def prevent; end
+  else
+    def dispatch
+      @id = `window.setTimeout(function() {
+        #{@function.call(*@arguments, &@block)};
+      }, 0)`
+    end
+
+    def prevent
+      `window.clearTimeout(#@id)`
+    end
+  end
 
   # Abort the immediate.
   def abort
@@ -89,5 +155,3 @@ class Promise
     }
   end
 end
-
-require 'browser/compatibility/immediate'
