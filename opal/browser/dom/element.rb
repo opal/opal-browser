94492ff345e248f3ee373a5e363a804c221127da
meh <meh@schizofreni.co>
2014-02-01 04:08:02 +0100

    dom/element: fix #css on IE6-7

--- opal/browser/dom/element.rb
+++ opal/browser/dom/element.rb
@@ -266,7 +266,7 @@ class Element < Node
     end
   elsif Browser.loaded? 'Sizzle'
     def css(path)
-      NodeSet.new(`Sizzle(path, #@native)`)
+      NodeSet[`Sizzle(path, #@native)`.map { |x| Native(x) }]
     rescue
       NodeSet[]
     end
