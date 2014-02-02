8713848559e04af445aaaaa90f2f2d55c2551a2b
meh <meh@schizofreni.co>
2014-02-02 15:49:41 +0100

    dom/node_set: more cleanup

--- opal/browser/dom/node_set.rb
+++ opal/browser/dom/node_set.rb
@@ -2,20 +2,21 @@ module Browser; module DOM
 
 # Allows manipulation of a set of {Node}s.
 class NodeSet
+  # Create a new {NodeSet} from the given nodes.
+  #
+  # Note that the nodes are flattened and converted with DOM automatically,
+  # this means you can pass {NodeSet}s and {Native::Array}s as well.
   def self.[](*nodes)
-    new(nodes.flatten.map { |x| DOM(Native.convert(x)) })
+    new(nodes.flatten.map { |x| DOM(Native.convert(x)) }.uniq)
   end
 
-  def initialize(array)
-    @array = array
+  def initialize(literal)
+    @literal = literal
   end
 
-  def respond_to_missing?(name)
-    @array.respond_to?(name)
-  end
-
+  # Any other method will be called on every node in the set.
   def method_missing(name, *args, &block)
-    unless @array.respond_to? name
+    unless @literal.respond_to? name
       each {|el|
         el.__send__(name, *args, &block)
       }
@@ -23,9 +24,9 @@ class NodeSet
       return self
     end
 
-    result = @array.__send__ name, *args, &block
+    result = @literal.__send__ name, *args, &block
 
-    if `result === #@array`
+    if `result === #@literal`
       self
     elsif Array === result
       NodeSet.new(result)
@@ -34,60 +35,25 @@ class NodeSet
     end
   end
 
-  def dup
-    NodeSet.new(@array.dup)
-  end
-
+  # Create another {NodeSet} with all the nodes that match the given
+  # expression.
+  #
+  # @param expression [String] a CSS selector
+  #
+  # @return [NodeSet] the new {NodeSet} with the matching nodes
   def filter(expression)
-    NodeSet.new(@array.select { |node| node =~ expression })
+    @literal.select { |node| node =~ expression }
   end
 
-  def after(node)
-    last.after node
-  end
-
-  def at(path)
-    raise NotImplementedError
-  end
-
-  def at_css(*rules)
-    raise NotImplementedError
-  end
-
-  def at_xpath(*paths)
-    raise NotImplementedError
-  end
-
-  def before
-    first.before
-  end
-
+  # @!attribute [r] children
+  # @return [NodeSet] recursively collected children of the nodes in the set
   def children
-    result = NodeSet.new([])
-
-    each { |n| result.concat(n.children) }
-
-    result
+    map(&:children).flatten
   end
 
-  def css(*paths)
-    raise NotImplementedError
-  end
-
+  # Search for multiple selectors
   def search(*what)
     map { |n| n.search(*what) }.flatten.uniq
-  end
-
-  def to_a
-    @array.dup
-  end
-
-  def to_ary
-    self
-  end
-
-  def inspect
-    "#<DOM::NodeSet: #{@array.inspect[1 .. -2]}"
   end
 end
 
