caac4817eed6275ba2c1d62b7e639a00a5c42700
meh <meh@schizofreni.co>
2014-02-07 00:12:52 +0100

    canvas: remove leftover headers

--- opal/browser/canvas/gradient.rb
+++ opal/browser/canvas/gradient.rb
@@ -1,13 +1,3 @@
-#--
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#                    Version 2, December 2004
-#
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-#
-#  0. You just DO WHAT THE FUCK YOU WANT TO.
-#++
-
 module Browser; class Canvas
 
 class Gradient
