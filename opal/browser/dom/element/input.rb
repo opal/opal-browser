ebad6f64f10661914abf3c8c409415c89b22ec8c
meh <meh@schizofreni.co>
2014-08-18 19:54:03 +0200

    dom/element/input: return nil on #value

--- opal/browser/dom/element/input.rb
+++ opal/browser/dom/element/input.rb
@@ -2,7 +2,14 @@ module Browser; module DOM; class Element < Node
 
 class Input < Element
   def value
-    `#@native.value`
+    %x{
+      if (#@native.value == "") {
+        return nil;
+      }
+      else {
+        return #@native.value;
+      }
+    }
   end
 
   def value=(value)
