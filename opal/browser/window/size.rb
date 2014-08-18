2b6e5c236ea1ec6df74f8719cd48742ce8fb1386
meh <meh@schizofreni.co>
2014-08-18 20:03:07 +0200

    window/size: some cleanup and documentation

--- opal/browser/window/size.rb
+++ opal/browser/window/size.rb
@@ -1,19 +1,33 @@
 module Browser; class Window
 
+# Allows access and manipulation of the {Window} size.
 class Size
+  # @private
   def initialize(window)
     @window = window
     @native = window.to_n
   end
 
-  def set(what)
-    width  = what[:width]  || self.width
-    height = what[:height] || self.height
+  def set(*args)
+    if Hash === args.first
+      width, height = args.first.values_at(:width, :height)
+    else
+      width, height = args
+    end
+
+    width  ||= self.width
+    height ||= self.height
 
     `#@native.resizeTo(#{width}, #{height})`
 
     self
   end
+
+  # @!attribute width
+  # @return [Integer] the width of the window
+
+  # @!attribute height
+  # @return [Integer] the height of the window
 
   if Browser.supports? 'Window.outerSize'
     def width
