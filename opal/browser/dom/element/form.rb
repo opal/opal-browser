56c34f3e77f304a27d5964eb041b2fb1d4afac22
ahmadine <54514036+ahmadine@users.noreply.github.com>
2019-10-15 23:08:38 +0200

    dom/element/form: implement the DOM interface

--- opal/browser/dom/element/form.rb
+++ opal/browser/dom/element/form.rb
@@ -9,6 +9,32 @@ class Form < Element
   def form_data
     FormData.create(self)
   end
+
+  # Submit a form. This will fire a submit event.
+  def submit
+    `#@native.submit()`
+  end
+
+  # Reset a form. This will fire a reset event.
+  def reset
+    `#@native.reset()`
+  end
+
+  alias_native :action
+  alias_native :action=
+  alias_native :method
+  alias_native :method=
+  alias_native :target
+  alias_native :target=
+  alias_native :name
+  alias_native :name= 
+  alias_native :encoding
+  alias_native :encoding=
+
+  # Return a NodeSet containing all form controls belonging to this form element.
+  def controls
+    NodeSet[Native::Array.new(`#@native.elements`)]
+  end
 end
 
 end; end; end
